89da2627c8784c07e402ba3159db09d16e9aada0
Raphael9999 <rlouvrier@outlook.com>
2021-03-12 09:26:23 -0600

    refactor: create routine
    * transfer code into routine
    * remove comments moved to README
    * clarify remaining comments

--- CopyLocalFileToSharePoint.py
+++ CopyLocalFileToSharePoint.py
@@ -1,21 +1,18 @@
-#  How to copy a local file to an O365 SharePoint / Teams folder?
-#
-# * Open the SharePoint site in your browser, or your Teams in the Teams application
-# * Navigate to the receiving folder
-# * Clic the "Sync" button, and allow/agree with popup for OneDrive
-# * Wait a few second and check with your file explorer that the corresponding folder has been added  
-#   'C:\Users\YourUser\YourCompany\YourSharePointSite - YourDestinationFolder'
-
 import os     # to read env variable
 import shutil # for the copy itself
+
+def copy_file_to_sp(file, target_dir):
+    # Copy
+    shutil.copy(file, target_dir)
 
 # Source file you want to upload to O365
 LocalFile = './YourFile.txt' # relative path from work dir, you can of course use an absolute path
 
 # Destination
 userprofile = os.environ['USERPROFILE'] # return C:\Users\YourUser
+# you can overwrite DestinationDirectory with the absolute path of you sync directory 
 DestinationDirectory = userprofile + r'\YourCompany\YourSharePointSite - YourDestinationFolder' 
 
-# Copy
-shutil.copy(LocalFile, DestinationDirectory)
-# Allow a few seconds for your OneDrive to synchronize files with SharePoint
+# Call the routine and copy to sync folder
+copy_file_to_sp(LocalFile, DestinationDirectory)
+# Allow a few seconds for your OneDrive to synchronize the file with O365 SharePoint
